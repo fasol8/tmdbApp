855dd90abf98240b0b4b25c4fdd4c923201b3241
fasol8 <34354696+Fabritzi0@users.noreply.github.com>
2024-09-07 14:15:43 -0600

    Add TextButton to biography and years old in PersonDetail

--- app/src/main/java/com/sol/tmdb/presentation/person/PersonDetail.kt
+++ app/src/main/java/com/sol/tmdb/presentation/person/PersonDetail.kt
@@ -1,5 +1,8 @@
 package com.sol.tmdb.presentation.person
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,12 +18,14 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -30,6 +35,9 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import com.sol.tmdb.domain.model.person.Gender
 import com.sol.tmdb.domain.model.person.PersonDetail
+import java.time.LocalDate
+import java.time.Period
+import java.time.format.DateTimeFormatter
 
 @Composable
 fun PersonDetail(personId: Int, viewModel: PersonViewModel = hiltViewModel()) {
@@ -96,7 +104,7 @@ fun PersonCard(person: PersonDetail) {
                     )
                     Row {
                         Text(
-                            text = person.birthday + if (person.deathDay != null) "-${person.deathDay}" else "",
+                            text = "(${calculateAge(person.birthday)} years old) " + person.birthday + if (person.deathDay != null) "-${person.deathDay}" else "",
                             style = MaterialTheme.typography.bodySmall,
                             fontWeight = FontWeight.Light,
                             modifier = Modifier
@@ -128,9 +136,39 @@ fun PersonCard(person: PersonDetail) {
                         )
                     }
                     Spacer(modifier = Modifier.height(4.dp))
-                    Text(text = person.biography, style = MaterialTheme.typography.bodyMedium)
+                    BiographyText(person.biography)
                 }
             }
         }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun calculateAge(birthday: String, format: String = "yyyy-MM-dd"): Int {
+    val formatter = DateTimeFormatter.ofPattern(format)
+    val birthLocalDate = LocalDate.parse(birthday, formatter)
+    val currentDate = LocalDate.now()
+    return Period.between(birthLocalDate, currentDate).years
+}
+
+@Composable
+fun BiographyText(biography: String) {
+    var expand by remember { mutableStateOf(false) }
+    val showReadMore = biography.length > 200
+
+    Column(modifier = Modifier) {
+        Text(
+            text = if (expand) biography else biography.take(200),
+            style = MaterialTheme.typography.bodyMedium
+        )
+
+        if (showReadMore) {
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
+                TextButton(onClick = { expand = !expand }) {
+                    Text(text = if (expand) "Read less" else "Read more")
+                }
+            }
+        }
+
     }
 }
